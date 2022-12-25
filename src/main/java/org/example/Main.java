416504f644afb59c625ebd3775c6b896d043dbe9
Valerii Uskov <uskovvobdt@gmail.com>
2022-12-25 13:15:48 +0100

    get date

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Let's do it");
         System.out.println("I'm ...");
+        System.out.println(LocalDate.now());
     }
 }
