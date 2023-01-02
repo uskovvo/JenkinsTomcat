57daedf47a0ef5784e5a1e3f31f6c894f02aef41
Valerii Uskov <uskovvobdt@gmail.com>
2023-01-02 11:35:10 +0100

    test commit

--- src/main/java/com/example/jenkinstomcat/User.java
+++ src/main/java/com/example/jenkinstomcat/User.java
@@ -55,4 +55,13 @@ public class User {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User " +
+                "id " + id +
+                ", name " + name + '\'' +
+                ", surname " + surname + '\'' +
+                ", age " + age;
+    }
 }
